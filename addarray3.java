29dd46123242d597605215d7892f04cd6c50ee71
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-18 18:33:01 +0400

    Add files via upload

---
+++ addarray3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class addarray3{
+	public static void main(String[] arg){
+		int [] b=new int[5];
+		Scanner s=new Scanner(System.in);
+		for (int i=0; i<b.length; i++){
+			System.out.println("Enter " +(i+1));
+			b[i]=s.nextInt();
+		}
+		double avg=0;
+		for(int i=0; i<b.length; i++){
+			avg=avg+b[i];
+		}
+		avg=avg/b.length;
+		System.out.println("average:" +avg);
+	}
+}
